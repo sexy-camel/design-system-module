4243044c02958cdc969db78b6cedbe6c0cc714b2
jhpark27 <jhpark27@nm-metalabs.com>
2025-01-17 11:34:33 +0900

    Initial Commit

---
+++ DesignSystemModule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignSystemModule
+//
+//  Created by 박종혁 on 1/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignSystemModule/DesignSystemModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemModuleApp.swift
+//  DesignSystemModule
+//
+//  Created by 박종혁 on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
